f161a944bafbba4d1131eeadae5e61373eb0ecad
Adoherrent <96074280+Adoherrent@users.noreply.github.com>
2022-07-25 10:12:58 -0400

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,28 +1,31 @@
 clear
 sleep 2
-echo "Installing required files for "KenosV2" 10% [=>--------]"
+echo "Installing required files for "Fractured Net" 10% [=>--------]"
 sudo apt install nodejs
 clear
-echo "Installing required files for "KenosV2" 20% [==>-------]"
+echo "Installing required files for "Fractured Net" 20% [==>-------]"
 sudo apt install perl
 clear
-echo "Installing required files for "KenosV2" 30% [===>------]"
+echo "Installing required files for "Fractured Net" 30% [===>------]"
 sudo apt install cat
 clear
-echo "Installing required files for "KenosV2" 50% [=====>----]"
+echo "Installing required files for "Fractured Net" 50% [=====>----]"
 sudo apt install curl
 clear
-echo "Installing required files for "KenosV2" 70% [=======>--]"
+echo "Installing required files for "Fractured Net" 70% [=======>--]"
 sudo apt install wget python
 clear
-echo "Installing required files for "KenosV2" 90% [========>-]"
+echo "Installing required files for "Fractured Net" 80% [========>-]"
+sudo apt-get -y purge python2.7
+clear
+echo "Installing required files for "Fractured Net" 90% [========>-]"
 sleep 1
 clear
-echo "Installing required files for "KenosV2" 100% [==========]"
+echo "Installing required files for "Fractured Net" 100% [==========]"
 sleep 2
 clear
 echo "Cleaning up"
 sleep 2
 rm -rf installation.sh
 sleep 1
-python3 Kenos.py
+python2 server.py
