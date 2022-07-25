14baf6f58bfb3740d1777366d3ec1d3e534f7e4c
Adoherrent <96074280+Adoherrent@users.noreply.github.com>
2022-07-25 10:09:56 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,50 @@
+import hashlib, os
+
+resource_file = "database.sql"
+
+os.system("clear")
+
+def encode(username, password):
+    return "$%s::%s$" % (username, hashlib.sha256(password).hexdigest())
+
+
+def add_user(username, password):
+    if os.path.exists(resource_file):
+        with open(resource_file) as f:
+            if "$%s::" % username in f.read():
+                raise Exception("User already exists")
+    with open(resource_file, "w") as f:
+        print >> f, encode(username, password)
+    return username
+
+
+def check_login(username, password):
+    with open(resource_file) as f:
+        if encode(username, password) in f.read():
+            return username
+
+
+def create_username():
+    try:
+        username = add_user(raw_input("User: "),
+                            raw_input("Password: "))
+        print "Added User %s" % username
+    except Exception as e:
+        print "Failed to add user %s! ... user already exists??" % username
+
+
+def login():
+    if check_login(raw_input("User: "), raw_input("Password: ")):
+        print "Login Success"
+        os.system("python3 client.py")
+    else:
+        print "Nice try , try again next time."
+
+while True:
+    {
+        'c': create_username,
+        'l': login
+    }.get(
+        raw_input(
+            "\033[31m      -------------------\n      | \033[37m(C)reate User   \033[31m|\n      | \033[37m(L)ogin         \033[31m|\n      -------------------\n\033[32mlogin\033[37m@\033[31mfractured~ \033[37m").lower(),
+        login)()
